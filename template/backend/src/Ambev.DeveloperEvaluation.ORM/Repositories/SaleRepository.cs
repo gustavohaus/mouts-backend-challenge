5ae86bdf0dd34504d623fcd990aea804e702402c
Gustavo Haus <gustavohaus@gmail.com>
2025-11-20 21:11:05 -0300

    feat: implement repositories

---
+++ template/backend/src/Ambev.DeveloperEvaluation.ORM/Repositories/SaleRepository.cs
@@ -0,0 +1,66 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Ambev.DeveloperEvaluation.Domain.Enums;
+using Ambev.DeveloperEvaluation.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ambev.DeveloperEvaluation.ORM.Repositories
+{
+    public class SaleRepository : ISaleRepository
+    {
+        private readonly DefaultContext _context;
+
+        public SaleRepository(DefaultContext context)
+        {   
+            _context = context;
+        }
+
+        public async Task<Sale> CreateAsync(Sale sale, CancellationToken cancellationToken = default)
+        {
+            await _context.Sales.AddAsync(sale, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+            return sale;
+        }
+
+
+        public async Task<Sale?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+        {
+            return await _context.Sales
+                .Include(s => s.SaleProducts)
+                .ThenInclude(s => s.Product)
+                .FirstOrDefaultAsync(s => s.Id == id, cancellationToken);
+        }
+
+
+        /// <summary>
+        /// Updates an existing sale in the database
+        /// </summary>
+        /// <param name="sale">The sale to update</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns>The updated sale</returns>
+        public async Task<Sale> UpdateAsync(Sale sale, CancellationToken cancellationToken = default)
+        {
+            _context.Sales.Update(sale);
+            await _context.SaveChangesAsync(cancellationToken);
+            return sale;
+        }
+
+        /// <summary>
+        /// Deletes a sale from the database
+        /// </summary>
+        /// <param name="id">The unique identifier of the sale to delete</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns>True if the sale was deleted, false if not found</returns>
+        public async Task<bool> DeleteAsync(Sale sale, CancellationToken cancellationToken = default)
+        {
+
+            _context.Sales.Remove(sale);
+            await _context.SaveChangesAsync(cancellationToken);
+            return true;
+        }
+    }
+}
